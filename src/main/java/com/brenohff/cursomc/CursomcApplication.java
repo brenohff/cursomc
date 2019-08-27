4be594d0ce2b48a23ae9a3d9a0070b118117a91f
Breno Franco <breno.franco@maxipago.com>
2019-08-27 17:28:15 -0300

    Criação da classe produto; Associação @ManyToMany

--- src/main/java/com/brenohff/cursomc/CursomcApplication.java
+++ src/main/java/com/brenohff/cursomc/CursomcApplication.java
@@ -1,13 +1,46 @@
 package com.brenohff.cursomc;
 
+import com.brenohff.cursomc.entities.CategoryEntity;
+import com.brenohff.cursomc.entities.ProductEntity;
+import com.brenohff.cursomc.repositories.CategoryRepository;
+import com.brenohff.cursomc.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		CategoryEntity categoryEntity1 = new CategoryEntity(null, "Informática");
+		CategoryEntity categoryEntity2 = new CategoryEntity(null, "Escritório");
+
+		ProductEntity p1 = new ProductEntity(null, "Computador", 2000.0);
+		ProductEntity p2 = new ProductEntity(null, "Impressora", 800.0);
+		ProductEntity p3 = new ProductEntity(null, "Mouse", 80.0);
+
+		categoryEntity1.getProductList().addAll(Arrays.asList(p1, p2, p3));
+		categoryEntity2.getProductList().addAll(Arrays.asList(p2));
+
+		p1.getCategoryList().addAll(Arrays.asList(categoryEntity1));
+		p2.getCategoryList().addAll(Arrays.asList(categoryEntity1, categoryEntity2));
+		p3.getCategoryList().addAll(Arrays.asList(categoryEntity1));
+
+		categoryRepository.saveAll(Arrays.asList(categoryEntity1, categoryEntity2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+	}
 }
--- src/main/java/com/brenohff/cursomc/entities/ProductEntity.java
+++ src/main/java/com/brenohff/cursomc/entities/ProductEntity.java
@@ -11,19 +11,24 @@ import java.util.List;
 @NoArgsConstructor
 @Data
 @Entity
-public class CategoryEntity implements Serializable {
+public class ProductEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    private Double price;
 
-    @ManyToMany(mappedBy = "categoryList")
-    private List<ProductEntity> productList = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(name = "PRODUCT_CATEGORY",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private List<CategoryEntity> categoryList = new ArrayList<>();
 
-    public CategoryEntity(Integer id, String name) {
+    public ProductEntity(Integer id, String name, Double price) {
         this.id = id;
         this.name = name;
+        this.price = price;
     }
 }
