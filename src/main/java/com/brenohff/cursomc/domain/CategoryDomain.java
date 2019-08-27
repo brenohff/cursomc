04a9595223c75aef50a729d50a6553081931fe68
Breno Franco <breno.franco@maxipago.com>
2019-08-27 16:02:03 -0300

    Ajustes no properties para mostrar sql; Criando a entidade Category

--- src/main/java/com/brenohff/cursomc/domain/CategoryDomain.java
+++ src/main/java/com/brenohff/cursomc/domain/CategoryDomain.java
@@ -3,13 +3,20 @@ package com.brenohff.cursomc.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@Entity
 public class CategoryDomain implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 }
